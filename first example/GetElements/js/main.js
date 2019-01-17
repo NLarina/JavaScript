ef9dbf41e424956a44b5ed06a4a981b7c85ede90
Natasha <funkytimebruno@gmail.com>
2019-01-18 00:28:48 +0300

    first commit

---
+++ first example/GetElements/js/main.js
@@ -0,0 +1,27 @@
+let btnStart= document.getElementById("start");
+
+let MainNodeValueProperty = document.querySelector(".result-table");
+
+let PropertyValue =  MainNodeValueProperty.querySelectorAll("[class*='-value']");
+
+
+
+let ExpensesItems =  document.getElementsByClassName("expenses-item");
+let Btn = document.getElementsByTagName("button");
+
+//кнопки
+let chooseExpenses =  Btn[0], 
+ optionalExpenses =  Btn[1], 
+ countBudget =  Btn[2]; 
+
+ document.querySelector(".choose-income");
+ document.querySelector("#savings");
+ console.log( document.querySelector("#savings"));
+ document.querySelector(".choose-sum");
+ document.querySelector(".choose-percent");
+ document.querySelector(".year-value");
+ document.querySelector(".month-value");
+ document.querySelector(".day-value");
+
+
+
